1a1595111cfc9cf39e6db7f10293851332836e45
haigang-shushu <haigangshushu@gmail.com>
2022-09-01 23:29:41 +0800

    提交 05 06 07

---
+++ 05-06 基础数据类型变量的定义和常用操作.py
@@ -0,0 +1,15 @@
+# 数据类型
+# 整数 （int） 8
+# 浮点数（float） 8.8
+# 字符串（str） “8” “Python”
+# 布尔值（bool）True False
+
+# a 为变量名称; = 为变量赋值; 123 为变量得到的值
+a = 123
+print(a)
+
+# 大驼峰: 每一个单词的首字母都大写 FirstName LastName
+# 小驼峰: 第一个单词以小写字母开始，后续单词的首字母大写 firstName lastName
+# py 一般使用小驼峰加下划线的方式 bang_width
+# 变量名包括字母，数字，下划线，但是数字不能作为开头
+

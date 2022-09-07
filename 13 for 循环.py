11a31ced17c9b3c0f28d75037571e298ae82b326
haigang-shushu <haigangshushu@gmail.com>
2022-09-07 23:38:40 +0800

    提交 14 while 循环

--- 13 for 循环.py
+++ 13 for 循环.py
@@ -6,4 +6,4 @@ for cz in chinese_zodiac:
 for i in range(1, 13):
     print(i)
 for year in range(2000, 2019):
-    print('%s 年的生肖是 %s' %(year, chinese_zodiac[year % 12]))
+    print('%s 年的生肖是 %s' % (year, chinese_zodiac[year % 12]))

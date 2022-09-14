8d600499caa8966d84017f8d318ab4cd5cc2f794
haigang-shushu <haigangshushu@gmail.com>
2022-09-14 23:19:14 +0800

    提交 21 异常检测和处理

--- 21 异常的检测和处理.py
+++ 21 异常的检测和处理.py
@@ -1,6 +1,23 @@
-
+# 异常是在出现错误时采用正常控制流以外的动作
+# 异常处理的一般流程是:检测到错误,引发异常;对异常进行捕获的操作
+# try:
+#     <监控异常>
+# except Exception[, reason]:
+#     <异常处理代码>
+# finally:
+#     <无论异常是否发生都执行>
 # year = int(input('input year:'))
+# try:
+#     year = int(input('input year:'))
+# except ValueError:
+#     print('年份要输入数字:')
+
+# a = 123
+# a.append()
+
 try:
-    year = int(input('input year:'))
-except ValueError:
-    print('年份要输入数字:')
+    a = open('name.txt')
+except Exception as e:
+    print(e)
+finally:
+    a.close()

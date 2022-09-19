dccb55712f8d46cb1ea9357d0273422dfe7b3800
haigang-shushu <haigangshushu@gmail.com>
2022-09-19 22:22:26 +0800

    25 函数的迭代器和生成器

--- 25 函数的迭代器与生成器.py
+++ 25 函数的迭代器与生成器.py
@@ -8,3 +8,12 @@ print(next(it))
 
 for i in range(10, 20, 2):
     print(i)
+
+def frange(start, stop, step):
+    x = start
+    while x < stop:
+        yield x
+        x += step
+
+for i in frange(10, 20, 0.5):
+    print(i)

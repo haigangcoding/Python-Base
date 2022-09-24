b4934d05b4853dfb19e4c62983dadb30c2153fbd
haigang-shushu <haigangshushu@gmail.com>
2022-09-24 09:03:49 +0800

    33 模块的定义

---
+++ 33 模块的定义.py
@@ -0,0 +1,12 @@
+# 模块是在代码量变得相当大之后，为了将需要重复使用的有组织的代码段放在一起
+# 这部分代码可以附加到现有的程序中，附件的过程叫做导入(import)
+# 导入模块的一般写法
+# import 模块名称
+# from 模块名称 import 方法名
+import os
+# import time
+# time.sleep()
+def print_me():
+    print('me')
+
+print('me')

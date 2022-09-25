7317c9d227c5e29a64b9b1c79d16f0e43ef1cfc4
haigang-shushu <haigangshushu@gmail.com>
2022-09-25 22:32:54 +0800

    34pep8编码规范

--- 34PEP8 编码规范.py
+++ 34PEP8 编码规范.py
@@ -1,2 +1,10 @@
 # https://www.python.org/dev/peps/pep-0008/
 # pip3 install autopep8
+
+# pip 换源
+#
+# 1、文件管理器文件路径地址栏敲：%APPDATA% 回车，快速进入 C:\Users\用户\AppData\Roaming 文件夹中
+#
+# 2、新建 pip 文件夹并在文件夹中新建 pip.ini 配置文件
+#
+# 3、新增 pip.ini 配置文件内容

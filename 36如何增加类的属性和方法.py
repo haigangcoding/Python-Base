539d6ebda5d3289e80c22a706b605189f9d982d6
haigangcoding <haigangshushu@gmail.com>
2022-09-28 22:47:32 +0800

    36增加类的属性和方法

--- 36如何增加类的属性和方法.py
+++ 36如何增加类的属性和方法.py
@@ -1,14 +1,21 @@
 
-# ¶¨ÒåÒ»¸öÀà
+# å®šä¹‰ä¸€ä¸ªç±»
 class Player():
     def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-        # ¶¨ÒåÒ»¸ö·½·¨
+
+        # å®šä¹‰ä¸€ä¸ªæ–¹æ³•
     def print_role(self):
         print('%s: %s' %(self.name, self.hp))
 
 class Monster():
-    # ¶¨Òå
 
 
+
+
+# ç±»çš„å®žä¾‹åŒ–
+user1 = Player('tom', 100)
+user2 = Player('jerry', 90)
+user1.print_role()
+user2.print_role()

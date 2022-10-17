10be9b5b2d7734640afaff5f625368597e82555d
haigangshushu@gmail.com <haigangshushu@gmail.com>
2022-10-17 21:26:29 +0800

    39 多线程编程的定义

--- 39多线程编程的定义.py
+++ 39多线程编程的定义.py
@@ -1,2 +1,33 @@
-def myThread(arg1, arg2):
-    print('%s %s' %(arg1, arg2))
+# import threading
+# import time
+# from threading import current_thread
+#
+#
+# def myThread(arg1, arg2):
+#     print(current_thread().getName(), 'start')
+#     print('%s %s' %(arg1, arg2))
+#     time.sleep(5)
+#     print(current_thread().getName(), 'stop')
+#
+#
+# for i in range(1, 6, 1):
+#     # t1 = myThread(i, i + 1)
+#     t1 = threading.Thread(target=myThread, args=(i, i + 1))
+#     t1.start()
+#
+# print(current_thread().getName(), 'end')
+
+import threading
+from threading import current_thread
+
+class Mythread(threading.Thread):
+    def run(self):
+        print(current_thread().getName(), 'start')
+        print('run')
+        print(current_thread().getName(), 'stop')
+
+t1 = Mythread()
+t1.start()
+t1.join()
+
+print(current_thread().getName(), 'end')

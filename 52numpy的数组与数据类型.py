8c9f0de9b2a9dade99b91297ba19daacf69f661c
haigangshushu@gmail.com <haigangshushu@gmail.com>
2022-10-22 09:39:23 +0800

    52numpy的数组与数据类型

---
+++ 52numpy的数组与数据类型.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# 定义一个列表
+arr1 = np.array([2, 3, 4])
+
+# 输出列表
+print(arr1)
+
+# dtype 返回数据元素的数据类型（int、float等）
+print(arr1.dtype)
+
+arr2 = np.array([1.2, 2.3, 3.4])
+
+print(arr2)
+print(arr2.dtype)

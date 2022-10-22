9443cd273e4614556b95eaa0545447cb542ecdb5
haigangshushu@gmail.com <haigangshushu@gmail.com>
2022-10-22 10:03:33 +0800

    53 numpy数组和标量的计算

---
+++ 53numpy数组和标量的计算.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# arr1 = np.array([2, 3, 4])
+#
+# arr2 = np.array([1.2, 2.3, 3.4])
+#
+# print(arr1 + arr2)
+# print(arr2 * 10)
+
+# 二维数组
+# data = [[1, 2, 3], [4, 5, 6]]
+# arr3 = np.array(data)
+# print(arr3)
+# print(arr3.dtype)
+
+# 定义一个全为 0 的矩阵
+print(np.zeros((3, 5)))
